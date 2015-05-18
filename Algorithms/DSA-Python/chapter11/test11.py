12d25d893fbd51ea23f017fc4b1a283dffa794f6
linx <plinux@qq.com>
2015-05-19 03:05:38 +0800

    avl.

--- Algorithms/DSA-Python/chapter11/test11.py
+++ Algorithms/DSA-Python/chapter11/test11.py
@@ -3,6 +3,7 @@ __author__ = 'linx'
 from unittest import TestCase
 from unittest import main
 from inc.trees import BinarySearchTree
+from inc.trees import AVLTree
 
 class cpt11(TestCase):
     def test_binarysearchtree(self):
@@ -24,6 +25,30 @@ class cpt11(TestCase):
         bst.remove(10)
         self.assertEqual(bst._root._item._key, 8)
         #bst.travel_inorder()
+
+    def test_avltree(self):
+        item = lambda x : [x, chr(ord('A') + x)]
+        avl1, avl2 = AVLTree(), AVLTree()
+        avl3, avl4 = AVLTree(), AVLTree()
+        self.assertEqual(avl1.is_empty(), True)
+        for i in xrange(5, 0, -1):
+            avl1.insert(item(i)[0], item(i)[1])
+        self.assertEqual(avl1._root._item._value, "E")
+        for i in xrange(5):
+            avl2.insert(item(i)[0], item(i)[1])
+        self.assertEqual(avl2.is_empty(), False)
+        self.assertEqual(avl2._root._item._value, "B")
+
+        avl3.insert(item(5)[0], item(5)[1])
+        avl3.insert(item(1)[0], item(1)[1])
+        avl3.insert(item(2)[0], item(2)[1])
+        self.assertEqual(avl3._root._item._value, "C")
+
+        avl4.insert(item(5)[0], item(5)[1])
+        avl4.insert(item(9)[0], item(9)[1])
+        avl4.insert(item(6)[0], item(6)[1])
+        self.assertEqual(avl4._root._item._value, "G")
+
 
     pass
 
--- Algorithms/DSA-Python/inc/trees.py
+++ Algorithms/DSA-Python/inc/trees.py
@@ -115,6 +115,98 @@ class BinarySearchTree(TreeBase):
             self._travle_inorder(pos._left)
             print (pos._item._key, pos._item._value),
             self._travle_inorder(pos._right)
+    pass
+
+class AVLTree(TreeBase):
+    def __init__(self):
+        self._root = None
+
+    def is_empty(self):
+        return self._root == None
+
+    def _height(self, pos):
+        if pos == None:
+            return 0
+        else:
+            return max(self._height(pos._left), self._height(pos._right)) + 1
+
+    def insert(self, key, value):
+        if self.is_empty():
+            self._root = self._Node(self._Item(key, value), None, None, None)
+            return
+        self._insert(key, value, self._root, None)
+
+    def _insert(self, key, value, pos, parent):
+        if pos == None:
+            return self._Node(self._Item(key, value), parent, None, None)
+        elif key < pos._item._key:
+            pos._left = self._insert(key, value, pos._left, pos)
+            if self._height(pos._right) == self._height(pos._left) - 2:
+                if key < pos._left._item._key:
+                    #print "single left rotate"
+                    pos = self._single_left_rotate(pos)
+                else: # pos._left._item._key < key
+                    #print "double left rotate"
+                    pos._left = self._single_right_rotate(pos._left)
+                    pos = self._single_left_rotate(pos)
+            return pos
+        elif pos._item._key < key:
+            pos._right = self._insert(key, value, pos._right, pos)
+            if self._height(pos._left) == self._height(pos._right) - 2:
+                if pos._right._item._key < key:
+                    #print "single right rotate"
+                    pos = self._single_right_rotate(pos)
+                else: # key < pos._right._item._key
+                    #print "double right rotate"
+                    pos._right = self._single_left_rotate(pos._right)
+                    pos = self._single_right_rotate(pos)
+            return pos
+        pass
+
+    def _single_left_rotate(self, pos):
+        parent = pos._parent
+        if parent != None:
+            if pos == parent._left:
+                parent._left = pos._left
+                pos._left = parent._left._right
+                parent._left._right = pos
+                #print parent, self._root._right._item._key
+                #print self._root._left._item._key
+                #print self._root._left._right._item._key
+                return parent._left
+            else: # pos == parent._right
+                parent._right = pos._left
+                pos._left = parent._right._right
+                parent._right._right = pos
+                return parent._right
+        else: # pos is root
+            self._root = self._root._left
+            pos._left = self._root._right
+            self._root._right = pos
+            return self._root
+
+    def _single_right_rotate(self, pos):
+        parent = pos._parent
+        if parent != None:
+            if pos == parent._left:
+                parent._left = pos._right
+                pos._right = parent._left._left
+                parent._left._left = pos
+                return parent._left
+            else:
+                parent._right = pos._right
+                pos._right = parent._right._left
+                parent._right._left = pos
+                return parent._right
+        else:
+            self._root = self._root._right
+            pos._right = self._root._left
+            self._root._left = pos
+            return self._root
+
+
+
+
 
 
     pass
