0955f9768156517a967e6a0bc9f1d5dc3b306c34
linx <plinux@qq.com>
2015-05-18 23:58:54 +0800

    bst.

--- Algorithms/DSA-Python/chapter11/test11.py
+++ Algorithms/DSA-Python/chapter11/test11.py
@@ -6,19 +6,25 @@ from inc.trees import BinarySearchTree
 
 class cpt11(TestCase):
     def test_binarysearchtree(self):
+        item = lambda x : [x, chr(ord('A') + x)]
         bst = BinarySearchTree()
         self.assertEqual(bst.is_empty(), True)
-        bst.insert(10)
+        bst.insert(item(10)[0], item(10)[1])
         self.assertEqual(bst.is_empty(), False)
-        bst.insert(5)
+        bst.insert(item(5)[0], item(5)[1])
         self.assertEqual(bst.find(5), True)
-        bst.insert(15)
-        bst.insert(1)
-        bst.insert(7)
-        bst.insert(6)
-        bst.insert(8)
-        #bst.remove(7)
-        bst.travel_inorder()
+        bst.insert(item(15)[0], item(15)[1])
+        bst.insert(item(1)[0], item(1)[1])
+        bst.insert(item(7)[0], item(7)[1])
+        bst.insert(item(6)[0], item(6)[1])
+        bst.insert(item(8)[0], item(8)[1])
+        #bst.travel_inorder()
+        bst.remove(1)
+        self.assertEqual(bst._root._left._left, None)
+        bst.remove(10)
+        self.assertEqual(bst._root._item._key, 8)
+        #bst.travel_inorder()
+
     pass
 
 if __name__ == '__main__':
--- Algorithms/DSA-Python/inc/trees.py
+++ Algorithms/DSA-Python/inc/trees.py
@@ -1,55 +1,108 @@
 __author__ = 'linx'
 
-class BinarySearchTree(object):
+class TreeBase(object):
+    class _Item(object):
+        def __init__(self, key, value):
+            self._key = key
+            self._value = value
+
     class _Node(object):
-        __slots__ = "_elem", "_left", "_right"
-        def __init__(self, elem, left, right):
-            self._elem = elem
+        def __init__(self, item, parent, left, right):
+            self._item = item
+            self._parent = parent
             self._left = left
             self._right = right
-        pass
+    pass
 
+class BinarySearchTree(TreeBase):
     def __init__(self):
         self._root = None
 
     def is_empty(self):
         return self._root == None
 
-    def find(self, elem):
+    def find(self, key):
         if self.is_empty():
             raise ValueError("Tree is empty.")
-        return self._find(elem, self._root)
+        return self._find(key, self._root)
 
-    def _find(self, elem, pos):
+    def _find(self, key, pos):
         if pos == None:
             return False
-        if elem == pos._elem:
+        if key < pos._item._key:
+            return self._find(key, pos._left)
+        elif key > pos._item._key:
+            return self._find(key, pos._right)
+        else: # elem == pos._elem:
             return True
-        elif elem < pos._elem:
-            return self._find(elem, pos._left)
-        else: # elem > pos._elem
-            return self._find(elem, pos._right)
 
-    def insert(self, elem):
-        self._root = self._insert(self._root, elem)
+    def insert(self, key, value):
+        if self.is_empty():
+            self._root = self._Node(self._Item(key, value), None, None, None)
+            return
+        self._insert(key, value, self._root, None)
 
-    def _insert(self, pos, elem):
+    def _insert(self, key, value, pos, parent):
         if pos == None:
-            return self._Node(elem, None, None)
-        elif elem < pos._elem:
-            pos._left =  self._insert(pos._left, elem)
+            return self._Node(self._Item(key, value), parent, None, None)
+        elif key < pos._item._key:
+            pos._left =  self._insert(key, value, pos._left, pos)
             return pos
-        elif elem > pos._elem:
-            pos._right = self._insert(pos._right, elem)
+        else: # key > pos._item._key:
+            pos._right = self._insert(key, value, pos._right, pos)
             return pos
 
-    def remove(self, elem):
+    def remove(self, key):
         if self.is_empty():
             raise ValueError("Tree is empty.")
-        #self._root = self._remove(self._root, elem)
+        if key != self._root._item._key:
+            self._remove(key, self._root)
+        else: # remove root key
+            if self._root._left == None:
+                if self._root._right == None:
+                    self._root = None
+                else:
+                    self._root = self._root._right
+            else:
+                if self._root._right == None:
+                    self._root = self._root._left
+                else:
+                    rpos = self._root._left
+                    while rpos._right != None:
+                        rpos = rpos._right
+                    self._root._item = rpos._item
+                    rpos = rpos._parent
+                    rpos._right = None
 
-    def _remove(self, pos, elem):
-        # error
+    def _remove(self, key, pos):
+        if key < pos._item._key:
+            self._remove(key, pos._left)
+        elif key > pos._item._key:
+            self._remove(key, pos._right)
+        else: # key == pos._item._key:
+            parent = pos._parent
+            if pos._left == None:
+                if pos == parent._left:
+                    parent._left = pos._right
+                else:
+                    parent._right = pos._right
+            elif pos._right == None:
+                if pos == parent._right:
+                    parent._left = pos._left
+                else:
+                    parent._right = pos._left
+            else:
+                rpos = pos._left
+                while rpos._right != None:
+                    print rpos._item._key
+                    rpos = rpos._right
+                pos._item = rpos._item
+                parent = rpos._parent
+                if rpos == parent._left:
+                    parent._left = None
+                else: # rpos = parent._right
+                    parent._right = None
+
         pass
 
     def travel_inorder(self):
@@ -60,10 +113,8 @@ class BinarySearchTree(object):
             return
         else:
             self._travle_inorder(pos._left)
-            print pos._elem
+            print (pos._item._key, pos._item._value),
             self._travle_inorder(pos._right)
-
-
 
 
     pass
